e1bd717a34db76713f9df65bab5a93f4d8cafe2d
Tamjeed Shariff <shariff6@my.yorku.ca>
2026-02-16 16:58:16 -0500

    favicon updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import favicon from './assets/logo.png';
+
+// Set the browser tab favicon to the bundled logo
+const setFavicon = (url) => {
+  const head = document.getElementsByTagName('head')[0];
+  let link = head.querySelector("link[rel*='icon']");
+  if (!link) {
+    link = document.createElement('link');
+  }
+  link.type = 'image/png';
+  link.rel = 'icon';
+  link.href = url;
+  head.appendChild(link);
+};
+setFavicon(favicon);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
